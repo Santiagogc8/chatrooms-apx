b72ac4161ce6e511c0404e4a92dce953ddf19e57
Santiago Guzman <guzmansantiago833@gmail.com>
2025-10-18 23:17:08 -0500

    first commit

--- api/database.ts
+++ api/database.ts
@@ -1,11 +1,17 @@
 // Instalamos la libreria firebase-admin
 import 'dotenv/config'
 import * as admin from "firebase-admin"; // La importamos
-import * as serviceAccountModule from "./key.json"; 
 
-// Esta línea de validación revisa si la clave está en 'default' (por problemas de compilación) 
-// o si es el objeto directo. Esto resuelve el problema de "invalid-credential".
-const serviceAccount = (serviceAccountModule as any).default || serviceAccountModule;
+// Cargamos el contenido JSON del secreto de Render
+const serviceAccountJson = process.env.FIREBASE_KEY_JSON;
+
+// Comprobamos si el contenido existe y lo parseamos
+if (!serviceAccountJson) {
+    throw new Error("FIREBASE_KEY_JSON no está configurada en Render");
+}
+
+// JSON.parse() convierte el string JSON en un objeto JavaScript
+const serviceAccount = JSON.parse(serviceAccountJson);
 
 admin.initializeApp({ // Inicializamos la app con el metodo initializeApp de admin
     credential: admin.credential.cert(serviceAccount), // Validamos la credencial con admin.credential.cert del JSON serviceAccount
